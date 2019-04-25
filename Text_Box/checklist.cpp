f6626fd6dca52fd19aed7c32ff7f4b76b7c0150e
nmax93 <38254068+nmax93@users.noreply.github.com>
2019-04-25 17:46:08 +0300

    Update checklist.cpp

--- Text_Box/checklist.cpp
+++ Text_Box/checklist.cpp
@@ -1,109 +1,37 @@
-#include "checklist.h"
+#include "checkbox.h"
 #include <iostream>
 #include <algorithm>
-Checklist::Checklist(short top, short left, std::string value) : 
-    top(top), value(value), left(left), hasBorder(true),
-    color(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY),
-    background(0),isEnabled(false),cpos(0) {}
-/*
-void Checklist::handleKeyboardEvent(KEY_EVENT_RECORD& event)
+
+checkbox::checkbox(short x, short y, std::string value)
+    : X(x), Y(y), value(value), isChecked(false),
+      color(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY),
+      background(0) {}
+
+void checkbox::toggle()
 {
-    if(!isEnabled) return;
-    if (!event.bKeyDown) return;
-
-    if (event.wVirtualKeyCode >= 0x30 && event.wVirtualKeyCode <= 0x5a)
-    {
-        pval=value;
-        if(value.length() < width){
-            value.insert(cpos,1,event.uChar.AsciiChar);
-            cpos++;
-            if(cpos>width-1)cpos=width-1;
-            draw();
-        }
-    }
-
-    if (event.wVirtualKeyCode == VK_LEFT || event.wVirtualKeyCode == VK_RIGHT)
-    {
-        int textWidth = value.length();
-        CONSOLE_SCREEN_BUFFER_INFO info;
-        auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
-        GetConsoleScreenBufferInfo(handle, &info);
-        auto offset = info.dwCursorPosition.X - this->left - 1;
-
-        if (offset > 0 && event.wVirtualKeyCode == VK_LEFT)
-        {
-            SetConsoleCursorPosition(handle,{info.dwCursorPosition.X - 1, info.dwCursorPosition.Y });
-            if(cpos > 0)
-                cpos--;
-        }
-
-        if (offset < textWidth && event.wVirtualKeyCode == VK_RIGHT)
-        {
-            SetConsoleCursorPosition(handle,{info.dwCursorPosition.X + 1, info.dwCursorPosition.Y });
-            if(cpos < textWidth){
-                cpos++;
-                if(cpos>width-1) cpos=width-1;
-            }
-        }
-    }
-
-    if (event.wVirtualKeyCode == VK_BACK)
-    {
-        pval=value;
-        if(value.length() > 0){
-            value.pop_back();
-            cpos--;
-        }
-        if(cpos < 0) cpos = 0;
-        if(pval != value)draw();
-    }
-}
-void Checklist::handleMouseEvent(MOUSE_EVENT_RECORD& event){
-    COORD c= event.dwMousePosition;
-    if(c.X >= left && c.X <= left+width+1 && c.Y >= top && c.Y <= top+3){
-        isEnabled = true;
-        auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleCursorPosition(handle, {top,c.X});
-        cpos=c.X-left;
-        if(cpos > value.length()) cpos = value.length();
-        draw();
-    }
-    else {
-    if(isEnabled)  draw();    
-        isEnabled = false;
-    }
-}
-*/
-void Checklist::drawBorder(COORD coord){
-    auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
-     std::cout << (char) 0xda;
-    for (int i=0; i<width; i++)
-        std::cout << (char) 0xc4;
-    std::cout << (char) 0xbf; //making upper right corner
-
-    SetConsoleCursorPosition(handle, {coord.X, coord.Y+1}); // reseting cursor to cetner left
-    std::cout << (char) 0xb3; // making the left center collum
-    
-   // for (int i =0; i < width; i++)
-     //   std::cout << (i >= value.length() ? ' ' : value[i]); // here guttman is printing the word
-    
-    SetConsoleCursorPosition(handle, { left + width + 1, top + 1 }); // restting cursor to center right
-    std::cout << (char) 0xb3; // making the right ctner collum
-
-    SetConsoleCursorPosition(handle, { left, top + 1 }); // setting the crusor to down area, and making the down part
-    std::cout << (char) 0xc0;
-    for (int i = 0; i < width; i++)
-        std::cout << (char) 0xc4;
-    std::cout << (char) 0xd9;
+  isChecked = !isChecked;
+  draw();
 }
 
-void Checklist::draw()
+COORD getCursorPosition()
 {
-    COORD coord = { left, top };
-    auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(handle, coord);
-    SetConsoleTextAttribute(handle, color | (background << 4));
-    drawBorder(coord);
-    SetConsoleCursorPosition(handle, { left + 1 + cpos, top + 1 });
+  CONSOLE_SCREEN_BUFFER_INFO info;
+  auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  GetConsoleScreenBufferInfo(handle, &info);
+  return info.dwCursorPosition;
 }
 
+void checkbox::draw()
+{
+  COORD originalCursorPos = getCursorPosition();
+  COORD coord = {X, Y};
+  auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleCursorPosition(handle, coord);
+
+  if (isChecked)
+    std::cout << "X\t" + value << std::endl;
+  else
+    std::cout << "O\t" + value << std::endl;
+
+  SetConsoleCursorPosition(handle, originalCursorPos);
+}
